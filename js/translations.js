42fd743600a078bc54c6df57fd965db42416b583
simoMortada1 <52280042+simoMortada1@users.noreply.github.com>
2022-10-02 13:14:24 +0100

    Add files via upload

---
+++ js/translations.js
@@ -0,0 +1,71 @@
+/*
+*******************************************************************************
+*
+*	Translations for the Vicidial WebRTC Phone
+*
+*	Copyright (C) 2018  Christian Cabrera
+*	Version 2.1.0
+*
+*	This program is free software: you can redistribute it and/or modify
+*	it under the terms of the GNU Affero General Public License as
+*	published by the Free Software Foundation, either version 3 of the
+*	License, or (at your option) any later version.
+*
+*	This program is distributed in the hope that it will be useful,
+*	but WITHOUT ANY WARRANTY; without even the implied warranty of
+*	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*	GNU Affero General Public License for more details.
+*
+*	You should have received a copy of the GNU Affero General Public License
+*	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+*******************************************************************************
+*/
+
+var vici_translations = {
+	// Original english language
+	en: {
+		registered: 	'Registered',
+		unregistered:	'Unregistered',
+		connecting: 	'Connecting...',
+		disconnected: 	'Disconnected',
+		connected: 		'Connected',
+		register_failed: 'Reg. failed',
+		incall: 		'Incall',
+		ringing: 		'Ringing',
+		attempting: 	'Attempting',
+		send: 			'Send',
+		call_hangup: 	'Call hungup',
+		webrtc_error:	'Something went wrong with WebRTC. Either your browser does not support the necessary WebRTC functions, you did not allow your browser to access the microphone, or there is a configuration issue. Please check your browsers error console for more details. For a list of compatible browsers please vist http://webrtc.org/',
+	},
+	// Spanish - Español
+	es: {
+		registered: 	'Registrado',
+		unregistered:	'No registrado',
+		connecting: 	'Conectando...',
+		disconnected: 	'Desconectado',
+		connected: 		'Conectado',
+		register_failed: 'Reg. fallido',
+		incall: 		'En llamada',
+		ringing: 		'Timbrando',
+		attempting: 	'Intentando',
+		send: 			'Enviar',
+		call_hangup: 	'Llamada colgada',
+		webrtc_error:	'Algo salió mal con WebRTC. Su navegador no es compatible con WebRTC, no ha autorizado que su navegador acceda al micrófono, o existe un problema en la configuración. Por favor, revise la consola de error de su navegador para obtener más detalles. Para revisar la lista de navegadores compatibles, visite http://webrtc.org',
+	},
+	// German - Deutsch
+	de: {
+		registered: 	'registriert',
+		unregistered:	'nicht registriert',
+		connecting: 	'verbinde...',
+		disconnected: 	'unterbrochen',
+		connected: 		'verbunden',
+		register_failed: 'Reg. fehlerhaft',
+		incall: 		'Anruf',
+		ringing: 		'klingeln',
+		attempting: 	'versuche',
+		send: 			'senden',
+		call_hangup: 	'Anruf aufgelegt',
+		webrtc_error:	'Etwas ist mit WebRTC schief gelaufen. Entweder unterstützt Ihr Browser die notwendigen WebRTC-Funktionen nicht, Sie haben Ihrem Browser nicht erlaubt, auf das Mikrofon zuzugreifen, oder es gibt ein Konfigurationsproblem. Bitte überprüfen Sie die Fehlerkonsole Ihres Browsers für weitere Einzelheiten. Für eine Liste der kompatiblen Browser besuchen Sie bitte http://webrtc.org/',
+	}
+}
